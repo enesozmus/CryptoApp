b00caa88657962264e95c889775f6be421b2676f
enesozmus <ensozmus@gmail.com>
2024-05-25 13:04:34 +0300

    Downloading coins using a View Model and Data Service class

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -5,21 +5,29 @@
 //  Created by enesozmus on 25.05.2024.
 //
 
+import Combine
 import Foundation
 
 // ✅ As we move around this app you'll see most of the screens in this app correlate to the same data source.
 // ✅ And because of that we're going to share this view model across a lot of the views that we make in our app.
-// go to root folder 
+// go to root folder
 class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private let coinService: CoinDataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        coinService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }

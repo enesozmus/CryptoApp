c08668d5b5810a04e3fac03a1659683cbd1fb433
enesozmus <ensozmus@gmail.com>
2024-05-29 01:21:44 +0300

    Adding animated launch screen

--- CryptoApp/Extensions/Color.swift
+++ CryptoApp/Extensions/Color.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 extension Color {
     static let theme = ColorTheme()
+    static let launchTheme = LaunchTheme()
 }
 struct ColorTheme {
     let accent = Color("AccentColor")
@@ -17,4 +18,8 @@ struct ColorTheme {
     let green = Color("CustomGreenColor")
     let red = Color("CustomRedColor")
     let secondaryText = Color("SecondaryTextColor")
+}
+struct LaunchTheme {
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
 }
